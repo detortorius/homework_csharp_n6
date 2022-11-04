c3cab7223203c435df77e4bd00d9f7d36acbf474
detortorius <detortorius@gmail.com>
2022-11-04 11:33:48 +0300

    Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+bool stop = false;
+int num = 0;
+int abzero = 0;
+
+while(!stop) // !false = true; !true = false;
+{
+    Console.WriteLine("Введите число");
+    num = int.Parse(Console.ReadLine() ?? "");
+    if(num > 0)
+    abzero++;
+
+    Console.WriteLine("Продолжить вводить числа? y/n");
+    string result = Console.ReadLine();
+    if(result == "y")
+        stop = false;
+    else
+        stop = true;
+    }
+    Console.WriteLine($"Количество чисел, введенных пользователем, которые больше 0 = {abzero}");
